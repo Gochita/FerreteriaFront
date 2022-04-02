dc3d3a19e54c1c89abcc66d9de7bb13364b4aa28
tamagochita <mevere66@gmail.com>
2022-04-02 09:58:34 -0500

    Navbar con routas creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        navbar...
+        <Navbar/>
         <Routes>
           <Route path="/" element={<div>Inicio... </div>} />
           <Route path="/login" element={<div>Login... </div>} />
