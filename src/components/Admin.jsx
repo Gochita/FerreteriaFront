09c202a7206476da4fe0b1a94bedf6e13ca5f791
tamagochita <mevere66@gmail.com>
2022-04-03 18:23:10 -0500

    Componente clientes creado

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
-import { getAuth } from "../firebase/auth";
+import { getAuth } from "firebase/auth";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import fire from "../firebase";
+import Clientes from "./Clientes";
 
 const auth = getAuth(fire);
 const Admin = () => {
   const [user, setUser] = useState(null);
+
   const navigate = useNavigate();
+
+  const handlerCliente = () => {
+    navigate("/admin/clientes");
+  };
+
   React.useEffect(() => {
     if (auth.currentUser) {
       console.log("existe un usuario");
+      setUser(auth.currentUser);
     } else {
       console.log("no existe el usuario");
       navigate("/login");
@@ -17,7 +25,19 @@ const Admin = () => {
   }, [navigate]);
   return (
     <div>
-      <h2>Ruta protegida</h2>
+      {user && <h3>{user.email}</h3>}
+
+      <button
+        className="btn btn-dark btn-sm  "
+        type="button"
+        style={{ margin: "0 auto" }}
+        onClick={handlerCliente}
+      >
+        Clientes
+      </button>
+      <Routes>
+        <Route path="/clientes" element={<Clientes />} />
+      </Routes>
     </div>
   );
 };
