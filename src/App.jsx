2a655aa277e870e057a96b80f875e5a0884e71d1
tamagochita <mevere66@gmail.com>
2022-04-03 22:03:22 -0500

    Ruta protegida para perfil admin

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,44 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Admin from "./components/Admin";
+import fire from "./firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  return (
+  const auth = getAuth(fire);
+  const [firebaseUser, setFirebaseUser] = React.useState(false);
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        setFirebaseUser(user);
+      } else {
+        setFirebaseUser(null);
+      }
+    });
+  }, []);
+  return firebaseUser !== false ? (
     <Router>
       <div className="container">
-        <Navbar />
+        <Navbar firebaseUser={firebaseUser} />
+        <br />
         <Routes>
+          <Route path="/" element={<div>Inicio... </div>} />
           <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<div><Admin /></div>} />
-          <Route path="/" element={"Inicio..."} />
+          <Route path="/admin/*" element={<Admin />} />
+          <Route path="*" element={<h1>404 NotFoundPage</h1>} />
         </Routes>
       </div>
     </Router>
+  ) : (
+    <button className="btn btn-primary" type="button" disabled>
+      <span
+        class="spinner-grow spinner-grow-sm"
+        role="status"
+        aria-hidden="true"
+      ></span>
+      Loading...
+    </button>
   );
 }
 
