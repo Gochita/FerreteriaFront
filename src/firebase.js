8a4f9a6f09d09a33011483bad9be81e150c2b015
tamagochita <mevere66@gmail.com>
2022-04-04 14:00:08 -0500

    credenciales cambiadas

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,25 @@ import {getFirestore} from 'firebase/firestore'
 
 import 'firebase/auth'
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDCo3JeOBCvIYGMavpNsWxHspDZGqtdH-M",
+//     authDomain: "ferreteria-e7b82.firebaseapp.com",
+//     projectId: "ferreteria-e7b82",
+//     storageBucket: "ferreteria-e7b82.appspot.com",
+//     messagingSenderId: "44845894288",
+//     appId: "1:44845894288:web:17e81184b407b2acea6615",
+//     measurementId: "G-DVGXYRBMGX"
+//   };
+
+  // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDCo3JeOBCvIYGMavpNsWxHspDZGqtdH-M",
-    authDomain: "ferreteria-e7b82.firebaseapp.com",
-    projectId: "ferreteria-e7b82",
-    storageBucket: "ferreteria-e7b82.appspot.com",
-    messagingSenderId: "44845894288",
-    appId: "1:44845894288:web:17e81184b407b2acea6615",
-    measurementId: "G-DVGXYRBMGX"
-  };
+  apiKey: "AIzaSyBN1KshswGIMBUBPef7PdrIvMc4oUl-lkA",
+  authDomain: "ferreteria2-df493.firebaseapp.com",
+  projectId: "ferreteria2-df493",
+  storageBucket: "ferreteria2-df493.appspot.com",
+  messagingSenderId: "647239137482",
+  appId: "1:647239137482:web:3254c60fafaa48a26adc33"
+};
   
   // Initialize Firebase
   const fire = initializeApp(firebaseConfig);
